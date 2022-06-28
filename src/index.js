2a7d380eb9335f6f5846f6ba828147ff20dd9568
Pratima Singh <singhpratima21222@gmail.com>
2022-06-28 19:31:52 +0530

    Updated package.json

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router} from 'react-router-dom'
-import {RoomProvider} from './context'
+import { HashRouter as Router } from 'react-router-dom'
+import { RoomProvider } from './context'
 
 ReactDOM.render(
   <RoomProvider>
